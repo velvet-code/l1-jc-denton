8c8b4d40bcf80e0da0b945ed26ebb80640a5b0e4
Raik Ilves <r@raik.me>
2024-01-29 11:30:10 +0200

    Remove hotjar and heap analytics. Add GA4

--- header.php
+++ header.php
@@ -28,26 +28,17 @@
 	<meta name="msapplication-config" content="<?php echo esc_url( get_template_directory_uri() )?>/images/favicons/browserconfig.xml?v=PYEqxpp0KA">
 	<meta name="theme-color" content="#ffffff">
 
-	<script type="text/javascript">
-		window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
-		heap.load("173698523");
-	</script>
-
-	<!-- Hotjar Tracking Code for https://level1.ee -->
-	<script>
-			(function(h,o,t,j,a,r){
-					h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-					h._hjSettings={hjid:546891,hjsv:5};
-					a=o.getElementsByTagName('head')[0];
-					r=o.createElement('script');r.async=1;
-					r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-					a.appendChild(r);
-			})(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-	</script>
-
 	<?php wp_head(); ?>
 </head>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-2Z5B01ERCC"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
 
+	gtag('config', 'G-2Z5B01ERCC');
+	</script>
 	<?php
 	$banner_image = get_field( 'banner_image', 'options' );
 	$banner_url = get_field( 'banner_target_url', 'options' );
